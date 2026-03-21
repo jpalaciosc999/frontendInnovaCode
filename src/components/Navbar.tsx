79e0fe4a0ac756d1fefd2f5578a86d8f7616def0
mtezen1611 <mtezenl@miumg.edu.gt>
2026-03-21 09:06:54 -0600

    posicion

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -18,8 +18,8 @@ function Navbar() {
     }}>
       <NavLink to="/" style={style}>Inicio</NavLink>
       <NavLink to="/empleados" style={style}>Empleados</NavLink>
+        <NavLink to="/departamentos" style={style}>Departamentos</NavLink>
       <NavLink to="/puestos" style={style}>Puestos</NavLink>
-      <NavLink to="/departamentos" style={style}>Departamentos</NavLink>
       <NavLink to="/roles" style={style}>Roles</NavLink>
       <NavLink to="/tipoIngreso" style={style}>Tipo de Ingreso</NavLink>
     </nav>
