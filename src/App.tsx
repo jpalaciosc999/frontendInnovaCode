b004911519d13c5e3e1ee66c880daf6ee4267e28
daveg10va <dgarciayoc@gmail.com>
2026-04-08 22:34:01 -0600

    CRUD Marcaje

--- src/App.tsx
+++ src/App.tsx
@@ -17,6 +17,7 @@ import TipoIngresos from './components/tipoIngresos';
 import NominaDetallePage from './components/NominaDetallePage';
 import KPIPage from './components/KPIPage';
 import KPIResultadoPage from "./components/KPIResultadoPage";
+import MarcajePage from "./components/MarcajePage";
 
 function App() {
   return (
@@ -40,6 +41,7 @@ function App() {
         <Route path="/nomina-detalle" element={<NominaDetallePage />} />
         <Route path="/kpis" element={<KPIPage />} />
         <Route path="/kpi-resultado" element={<KPIResultadoPage />} />
+        <Route path="/marcajes" element={<MarcajePage />} />
       </Routes>
     </>
   );
